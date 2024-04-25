08604ef3affa970b8608e4b1d9580e63c348e040
Sean Otto <seanotto29@gmail.com>
2024-04-25 12:30:26 +0100

    added powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,34 @@
+# Shows navigable menu of all options when hitting Tab
+
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+#Adds dig as an Alias for Resolve-DnsName
+
+New-Alias -Name dig -Value Resolve-DnsName
+
+function Run-League {
+<#
+
+.SYNOPSIS
+	Starts League of Legends
+
+.PARAMETER DontExit
+	if present, will close the shell after league has been started.
+.INPUTS 
+	Bool
+
+#>
+	[CmdletBinding()] #<<-- This turns a regular function into an advanced function
+	param	(
+		[Alias("e")]
+		[ValidateNotNullOrEmpty()]
+		[switch]$DontExit 
+	)
+	start "C:\Riot Games\Riot Client\RiotClientServices.exe" -ArgumentList "--launch-product=league_of_legends", "--launch-patchline=live"
+	if (!$DontExit)
+		{ exit}	
+}
+
+# Adds lol as an Alias for new "Run-League" Function
+
+New-Alias -Name lol -Value Run-League
