345ccfcaffca050b58f9ccd9f921de50b345f9ab
Sean, Otto <seanotto29@gmail.com>
2024-06-22 02:35:23 +0100

    idek

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 # Shows navigable menu of all options when hitting Tab
 
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
 function Run-League {
 <#
@@ -31,6 +31,13 @@ New-Alias -Name lol -Value Run-League
 #Adds dig as an Alias for Resolve-DnsName
 
 New-Alias -Name dig -Value Resolve-DnsName
+
+$ENV:FZF_DEFAULT_OPTS=@"
+--preview `"bat --color=always --style=auto --line-range=:500 {}`"
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
+"@
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin.omp.json" | Invoke-Expression
 
